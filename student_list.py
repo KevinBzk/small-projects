02661ca7795210acacea4ab9b9a841592441dcf5
PinkerToad <kevinbiczkowski@gmail.com>
2023-05-30 19:24:52 +0200

    added a student list

---
+++ student_list.py
@@ -0,0 +1,22 @@
+#list of students
+
+
+lists = []
+
+def check_name(students):
+    for spalte in lists:
+        a = spalte
+        print(a)
+    
+
+b = 0
+
+while b == 0:
+    stud0 = input("Your name: ")
+    lists.append(stud0)
+    c = input("Another Student?\n")
+    if c == "yes":
+        b= 0 
+    elif c == "no":
+        b +=1 
+print(check_name(lists))
